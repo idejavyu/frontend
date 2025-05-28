0491844163ebd38866a4f59f5e9ca0fa6a1c0ebe
idejavyu <idejavyu@gmail.com>
2025-05-28 16:44:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://wiki-backend-0ou3.onrender.com';
 const elements = {
     logoLink: document.getElementById('logoLink'),
     searchInput: document.getElementById('searchInput'),
